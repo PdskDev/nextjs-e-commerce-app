02c8189a2204684dad7e4031b5331ed21fee6d38
NadetDev <nadetdev@gmail.com>
2024-03-05 21:56:03 +0100

    Add Layout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
+import Layout from './components/Layout';
+
 //import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+export default function App({ Component, pageProps }) 
+{
+  return (
+  <Layout>
+  <Toaster/>
+  <Component {...pageProps} />
+  </Layout>
+ );
 }
---
+++ src/pages/components/Layout.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import Head from 'next/head';
+
+
+const Layout = ({children}) => {
+    return (
+        <>
+        <Head>
+            <title>Shop Market | Your e-shop</title>
+        </Head>
+        </>
+    )
+}
+ 
+
+
+export default Layout;
